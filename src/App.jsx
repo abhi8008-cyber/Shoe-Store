c01c1308f12152cb36f1bb98aa7d40cef134afcd
abhi8008-cyber <abhisarakale2001@gmail.com>
2024-07-21 09:09:20 +0000

    code-push

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./App.css"
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+
+
+function App() {
+  return (
+    <React.Fragment>
+  <div>
+   <Navigation/>
+   <Hero/>
+  </div>
+    </React.Fragment>
+  )
+}
+
+export default App
